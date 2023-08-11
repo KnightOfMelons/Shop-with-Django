cda053d01d446b1de9d83a1c6e0e1def3791c8ef
KnightOfMelons <ilaprosmotrov@gmail.com>
2023-08-11 12:56:28 +0400

    Have made cart system, ended up making shop, have made a few products, now every user can check and buy stuff. Added delete system from cart too.

---
+++ shop/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from main import views
+from shop.views import ProductsListView, ProductsDetailView, add_item_to_cart, cart_view, CartDeleteItem, make_order
+
+urlpatterns = [
+    path('', ProductsListView.as_view(), name='shop'),
+    path('cart_view/', cart_view, name='cart_view'),
+    path('detail/<int:pk>/', ProductsDetailView.as_view(),
+         name='shop_detail'),
+    path('add-item-to-cart/<int:pk>', add_item_to_cart, name='add_item_to_cart'),
+    path('delete_item/<int:pk>', CartDeleteItem.as_view(), name='cart_delete_item'),
+    path('make-order/', make_order, name='make_order'),
+]
